23c4c9d7b3394baf6a71a03da29c637d54739967
Duy <duy@duys-mbp.home>
2018-10-31 09:32:59 -0700

    add press

--- craps_sim.py
+++ craps_sim.py
@@ -1,37 +1,47 @@
 import random
 from types import SimpleNamespace
 
-def rollDice(num, wager):
+def rollDice():
 	DICE_MIN = 1
 	DICE_MAX = 6
 	dice1 = random.randint(DICE_MIN,DICE_MAX)
 	dice2 = random.randint(DICE_MIN,DICE_MAX)
-	dsum = dice1 + dice2
+	return (dice1 + dice2)
+
+def getPayout(diceOutcome, selectedNum, wager):
+	if (diceOutcome == 7):
+		return wager * -1
 	
-	payout = 0
-	if (dsum == 7):
-		payout = wager * -1
-
-	elif (num == dsum):
-		if(num == 6 or num == 8):
-			payout = (wager / 6) * 7
+	elif (diceOutcome == selectedNum):
+		if(selectedNum == 6 or num == 8):
+			return (wager / 6) * 7
 		
-		elif(num == 5 or num == 9):
-			payout = (wager / 5) * 7
+		elif(selectedNum == 5 or selectedNum == 9):
+			return (wager / 5) * 7
 		
-		elif(num == 5 or num == 9):
-			payout = (wager / 5) * 9
+		elif(selectedNum == 4 or selectedNum == 10):
+			return (wager / 5) * 9
 	else:
-		payout = 0
-	return {'result': dsum, 'payout': payout}
+		return 0
+	
 
-# inputs
-bankRoll = 10000
-selectedNum = 6
-wager = 108
+def playGame(selectedNum, startingBet):
+	bet = startingBet
+	totalPL = 0
+	while rollResult != 7:
+		rollResult = rollDice()
+		totalPL += getPayout(rollResult, selectedNum, bet)
+		if(rollResult == selectedNum):
+			bet =  bet * 1.5
 
-for x in range(1,1000):
-	betOutcome = SimpleNamespace(**rollDice(selectedNum, wager))
-	bankRoll = bankRoll + betOutcome.payout
+	return totalPL
 
-print(bankRoll)
+
+
+bankRoll = 1000
+for i in range(1, 10):
+	pL = playGame(bankRoll, 6, 108)
+	print(pL)
+	bankRoll = bankRoll + pL
+
+print bankRoll
