ac8f4edea3c2999be26ae922ddbaea38cc95f43a
Duy <duy@duys-mbp.home>
2018-11-02 16:53:11 -0700

    multiple games

--- craps_sim.py
+++ craps_sim.py
@@ -17,51 +17,55 @@ def rollDice():
     return (dice1 + dice2)
 
 
-def getPayout(diceOutcome, selectedNum, wager):
-    if (diceOutcome == 7):
-        return wager * -1
-
-    elif (diceOutcome == selectedNum):
-        if (selectedNum == 6 or selectedNum == 8):
-            return (wager / 6) * 7
-
-        elif (selectedNum == 5 or selectedNum == 9):
-            return (wager / 5) * 7
-
-        elif (selectedNum == 4 or selectedNum == 10):
-            return (wager / 5) * 9
+def getPayout(diceOutcome, betAmount):
+    if diceOutcome == 6 or diceOutcome == 8:
+        return (betAmount / 6) * 7
+    elif diceOutcome == 5 or diceOutcome == 9:
+        return (betAmount / 5) * 7
+    elif diceOutcome == 4 or diceOutcome == 10:
+        return (betAmount / 5) * 9
     else:
         return 0
 
+
 def pressBet(b):
-    return b * 2
+    return b * 0.5
 
 def playGame(bets):
     gameBets = bets
-    gameTotal = sum(bets.values()) * -1
+    startBetTotal = sum(bets.values())
+    gameTotal = startBetTotal * -1
     print('starting with {}'.format(gameTotal))
 
     r = 0
-    while r != 7:
+    while r != 7: 
+        print('roll bets: {}'.format(gameBets))
         r = rollDice() 
         if r == 7:
-            print('seven out, loser')
+            print('seven out')
 
         elif (r in gameBets.keys()):
-            print('winner winner chicken dinner {}'.format(r))
-            gameTotal += gameBets[r]
-            #gameBets[r] = pressBet(gameBets[r])
+            payout = getPayout(r, gameBets[r])
+            print('{0} winner winner chicken dinner {1}'.format(r, payout))
 
+            gameTotal += payout
+            if gameTotal >= gameBets[r]: 
+                pressAmount = pressBet(gameBets[r])
+                gameBets[r] = roundDown(gameBets[r] + pressAmount, getBetMultiplier(r))
+                gameTotal = gameTotal - pressAmount
+                print('pressed bet: {0}'.format(gameBets[r]))
         else:
             print('push nobody wins or loses {}'.format(r))
 
+        print(gameTotal)
     return gameTotal
 
 
-
 bankRoll = 1000
-for i in range(1,10):
-    selectedNums = {6: 10, 8:10}
-    bankRoll += playGame(selectedNums)
-    print(bankRoll)
-print('final total {}'.format(bankRoll))
+
+for i in range(1, 100):
+    if bankRoll > 0:
+        selectedNums = {4: 100, 6: 108, 8:108}
+        bankRoll += playGame(selectedNums)
+        print(bankRoll)
+
