f1e3dd0bc75ad5d6d0d955049bf713ac8ed3da28
Duy <duy@duys-mbp.home>
2018-11-02 14:50:35 -0700

    multiple numbers selected

--- craps_sim.py
+++ craps_sim.py
@@ -1,69 +1,67 @@
 import random
-from types import SimpleNamespace
-
-def rollDice():
-	DICE_MIN = 1
-	DICE_MAX = 6
-	dice1 = random.randint(DICE_MIN,DICE_MAX)
-	dice2 = random.randint(DICE_MIN,DICE_MAX)
-	return (dice1 + dice2)
-
-def getPayout(diceOutcome, selectedNum, wager):
-	if (diceOutcome == 7):
-		return wager * -1
-	
-	elif (diceOutcome == selectedNum):
-		if(selectedNum == 6 or selectedNum == 8):
-			return (wager / 6) * 7
-		
-		elif(selectedNum == 5 or selectedNum == 9):
-			return (wager / 5) * 7
-		
-		elif(selectedNum == 4 or selectedNum == 10):
-			return (wager / 5) * 9
-	else:
-		return 0
-	
-def playGame(selectedNum, startingBet):
-	gamePL = 0
-	rollResult = 0
-	breakEven = 0
-	nextBetBrokeEven = False
-
-	bet = roundDown(startingBet, getBetMultiplier(selectedNum))
-
-	while rollResult != 7:
-		win = 0
-		rollResult = rollDice()
-		gamePL += getPayout(rollResult, selectedNum, bet)
-		if(rollResult == selectedNum):
-			if(breakEven >= startingBet):
-				if(nextBetBrokeEven == True):
-					nextBet = bet * 1.5
-					bet = roundDown(nextBet, getBetMultiplier(selectedNum))
-					nextBetBrokeEven = False
-				else:
-					nextBetBrokeEven = True
-			else:
-				breakEven += gamePL
-				gamePL = 0
-	return (gamePL + breakEven)
 
 def roundDown(n, divisor):
-	return n - (n % divisor)
+    return n - (n % divisor)
 
 def getBetMultiplier(n):
-	if (n == 6 or n == 8):
-		return 6
-	else: 
-		return 5
+    if (n == 6 or n == 8):
+        return 6
+    else:
+        return 5
+
+def rollDice():
+    DICE_MIN = 1
+    DICE_MAX = 6
+    dice1 = random.randint(DICE_MIN, DICE_MAX)
+    dice2 = random.randint(DICE_MIN, DICE_MAX)
+    return (dice1 + dice2)
+
+
+def getPayout(diceOutcome, selectedNum, wager):
+    if (diceOutcome == 7):
+        return wager * -1
+
+    elif (diceOutcome == selectedNum):
+        if (selectedNum == 6 or selectedNum == 8):
+            return (wager / 6) * 7
+
+        elif (selectedNum == 5 or selectedNum == 9):
+            return (wager / 5) * 7
+
+        elif (selectedNum == 4 or selectedNum == 10):
+            return (wager / 5) * 9
+    else:
+        return 0
+
+def pressBet(b):
+    return b * 2
+
+def playGame(bets):
+    gameBets = bets
+    gameTotal = sum(bets.values()) * -1
+    print('starting with {}'.format(gameTotal))
+
+    r = 0
+    while r != 7:
+        r = rollDice() 
+        if r == 7:
+            print('seven out, loser')
+
+        elif (r in gameBets.keys()):
+            print('winner winner chicken dinner {}'.format(r))
+            gameTotal += gameBets[r]
+            #gameBets[r] = pressBet(gameBets[r])
+
+        else:
+            print('push nobody wins or loses {}'.format(r))
+
+    return gameTotal
+
+
 
 bankRoll = 1000
-startBet = 108
-tries = 8
-for i in range(1, tries):
-	if (bankRoll >= startBet):
-		pL = playGame(6, startBet)
-		print("game: {}".format(pL))
-		bankRoll = bankRoll + pL
-print("final bankroll: {}".format(bankRoll))
+for i in range(1,10):
+    selectedNums = {6: 10, 8:10}
+    bankRoll += playGame(selectedNums)
+    print(bankRoll)
+print('final total {}'.format(bankRoll))
